74dc34abc21738009f3e803741999be586b37069
Oguntayo Semilogo <semilogooguntayo2020@gmail.com>
2025-10-30 02:36:00 +0000

    Fix: corrected naming  issue again *5

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import "./Contact.css";
 import message_icon from "../../assets/help button3.png";
 import Phone_icon from "../../assets/Mail_Icon-Black.png";
-import mail_icon from "../../assets/call_Icon-black.png";
+import mail_icon from "../../assets/call_Icon-Black.png";
 import address_icon from "../../assets/location_icon-Black.png";
 import white_arrow from "../../assets/arrow_forward_Icon-White.png";
 
